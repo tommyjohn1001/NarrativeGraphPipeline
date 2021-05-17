Fix error in module DataReading and replace attention mask in MemoryBased by module

modules/narrativepipeline/NarrativePipeline.py
@@ -41,7 +41,7 @@ class  NarrativePipeline(torch_nn.Module):
         # paras : [b, n_paras, seq_len_para, d_hid]
         # ans   : [b, seq_len_ans, d_hid]
 
-        print("Finish 1")
+        # print("Finish 1")
 
         ####################
         # Do reasoning with IAL
@@ -49,13 +49,15 @@ class  NarrativePipeline(torch_nn.Module):
         Y       = self.reasoning(ques, paras)
         # Y: [n_paras + 1, b, d_hid]
 
-        print("Finish 2")
+        # print("Finish 2")
 
         ####################
         # Generate answer with PGD
         ####################
         pred    = self.ans_infer(Y, ans, is_inferring)
         # pred: [b, seq_len_ans, d_vocab]
+
+        # print("Finish 3")
 
         return pred
 
@@ -143,13 +145,13 @@ class Trainer():
             for batch in iterator_train:
                 pred_result = []
 
-                ques        = batch['ques'].to(args.device)
+                ques        = batch['ques'].to(args.device).float()
                 ques_mask   = batch['ques_mask'].to(args.device)
-                ans1        = batch['ans1'].to(args.device)
+                ans1        = batch['ans1'].to(args.device).float()
                 ans1_mask   = batch['ans1_mask'].to(args.device)
                 ans1_ids    = batch['ans1_ids']
                 ans2_ids    = batch['ans2_ids']
-                paras       = batch['paras'].to(args.device)
+                paras       = batch['paras'].to(args.device).float()
                 paras_mask  = batch['paras_mask'].to(args.device)
 
                 optimizer.zero_grad()
@@ -158,12 +160,14 @@ class Trainer():
                                     paras, paras_mask, is_inferring=False)
 
 
-
-                for pred_, ans1_, ans2_ in zip(pred, ans1_ids, ans2_ids):
+                # This piece of code is to show tokens of pred during training
+                _, indices = torch.softmax(pred, dim=2).topk(k=1, dim=2)
+                indices = indices.squeeze(2)
+                for pred_, ans1_, ans2_ in zip(indices, ans1_ids, ans2_ids):
                     pred_result.append({
-                        'pred': ' '.join(self.vocab.itos(pred_)),
-                        'ans1': ' '.join(self.vocab.itos(ans1_)),
-                        'ans2': ' '.join(self.vocab.itos(ans2_))
+                        'pred': ' '.join(self.vocab.itos(pred_.tolist())),
+                        'ans1': ' '.join(self.vocab.itos(ans1_.tolist())),
+                        'ans2': ' '.join(self.vocab.itos(ans2_.tolist()))
                     })
 
                 with open("backup/pred_train.json", 'a+') as result_file:
@@ -248,7 +252,7 @@ class Trainer():
         ###############################
         # Defind model and associated stuffs
         ###############################
-        model       = NarrativePipeline(self.vocab).to(args.device)
+        model       = NarrativePipeline(self.vocab).to(args.device).float()
         optimizer   = AdamW(model.parameters(), lr=args.lr, weight_decay=args.w_decay, eps=1e-6)
         scheduler   = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=10, eta_min=0)
 
@@ -313,7 +317,7 @@ class Trainer():
         ###############################
         # Defind model and associated stuffs
         ###############################
-        model       = NarrativePipeline(self.vocab).to(args.device).to(args.device)
+        model       = NarrativePipeline(self.vocab).to(args.device).to(args.device).float()
         model       = self.load_model(model)
 
         ###############################
@@ -344,9 +348,9 @@ class Trainer():
                                         paras, paras_mask, is_inferring=True)
                     for pred_, ans1_, ans2_ in zip(pred, ans1_ids, ans2_ids):
                         pred_result.append({
-                            'pred': ' '.join(self.vocab.itos(pred_)),
-                            'ans1': ' '.join(self.vocab.itos(ans1_)),
-                            'ans2': ' '.join(self.vocab.itos(ans2_))
+                            'pred': ' '.join(self.vocab.itos(pred_.tolist())),
+                            'ans1': ' '.join(self.vocab.itos(ans1_.tolist())),
+                            'ans2': ' '.join(self.vocab.itos(ans2_.tolist()))
                         })
 
                     with open(PATH['prediction'], 'a+') as result_file:

modules/narrativepipeline/utils.py
@@ -172,24 +172,13 @@ class CustomDataset(Dataset):
 
         return sent_, sent_mask_, np.array(sent_ids_, dtype=np.int)
 
-    def process_ans_loss(self, sent:str, max_len: int):
-        sent_       = sent.lower().split(' ')
-
-        sent_       = self.vocab.stoi(sent_)
-
-        pad_tok_id  = self.nlp_bert.pad_token_id
-
-        sent_len_   = len(sent_)
-        sent_       = np.array(sent_ + [pad_tok_id]*(max_len - sent_len_), dtype=np.int)
-
-        return sent_
-
     def f_process_file(self, entries, queue, arg):
         for entry in entries.itertuples():
             ###########################
             # Process question
             ###########################
             ques, ques_mask, _  = self.process_sent(entry.question, args.seq_len_ques)
+            ques    = np.vstack(ques)
 
 
             ###########################
@@ -203,6 +192,8 @@ class CustomDataset(Dataset):
 
             ans1, ans1_mask, ans1_ids   = self.process_sent(answers[0], args.seq_len_ans)
             ans2, _, ans2_ids           = self.process_sent(answers[0], args.seq_len_ans)
+            ans1    = np.vstack(ans1)
+            ans2    = np.vstack(ans2)
 
 
             ###########################
@@ -219,6 +210,12 @@ class CustomDataset(Dataset):
                 sent, sent_mask, _ = self.process_sent(sent, args.seq_len_para)
                 paras.append(np.expand_dims(sent, axis=0))
                 paras_mask.append(np.expand_dims(sent_mask, axis=0))
+
+            # This piece of code pads zero tensor to 'paras' and 'paras_mask'
+            # in case no. paras is less than args.n_paras
+            for _ in range(args.n_paras - len(paras)):
+                paras.append(np.zeros((1, args.seq_len_para, args.d_embd)))
+                paras_mask.append(np.zeros((1, args.seq_len_para)))
 
             paras       = np.vstack(paras)
             paras_mask  = np.vstack(paras_mask)

modules/reasoning/MemoryBased.py
@@ -153,20 +153,22 @@ class Memory(torch_nn.Module):
         super().__init__()
 
         self.memory     = [None]*(args.n_paras)
-        self.attn_mask  = torch.rand((args.n_paras, 1, args.batch, 1),
-                                     device=args.device, requires_grad=True)
+        self.ones       = torch.ones((args.batch, args.n_paras), device=args.device)
 
         if check_exist(PATH['memory']):
             logging.info("Tensors of MemoryModule exist. Load them.")
 
-            backup  = torch.load(PATH['memory'], map_location="cuda:0")
-            self.memory     = backup['memory']
-            self.attn_mask  = backup['attn_memory']
+            self.memory = torch.load(PATH['memory'], map_location="cuda:0")
 
 
         self.linear1    = torch_nn.Linear(args.d_hid, args.d_hid, bias=False)
         self.linear2    = torch_nn.Linear(args.d_hid, args.d_hid, bias=False)
         self.linear_gate= torch_nn.Linear(args.d_hid, args.d_hid, bias=True)
+
+        self.attn_mask  = torch_nn.Sequential(
+            torch_nn.Linear(args.n_paras, args.n_paras, bias=False),
+            torch_nn.Softmax(dim=1)
+        )
 
 
     def forward(self, nth_para: int, ques_para: Tensor, output: Tensor):
@@ -195,24 +197,25 @@ class Memory(torch_nn.Module):
         self.forward(nth_para, ques_para, output)
 
     def get_final_memory(self):
-        attn_mask   = torch.softmax(self.attn_mask, dim=0)
+        attn_mask   = self.attn_mask(self.ones)
+        # [batch, n_paras]
+        attn_mask   = attn_mask.transpose(0, 1).view(args.n_paras, 1, args.batch, 1)
+        # [n_paras, 1, batch, 1]
         attn_mask   = attn_mask.repeat(1, args.seq_len_ques + args.seq_len_para, 1, args.d_hid)
-        # [n_paras, seq_len_ques + seq_len_para, b, d_hid]
+        # [n_paras, seq_len_ques + seq_len_para, batch, d_hid]
 
         memory_     = [t.unsqueeze(0) for t in self.memory]
         memory_     = torch.vstack(memory_)
-        # [n_paras, seq_len_ques + seq_len_para, b, d_hid]
+        # [n_paras, seq_len_ques + seq_len_para, batch, d_hid]
 
         return (memory_ * attn_mask).sum(dim=0)
+        # [seq_len_ques + seq_len_para, batch, d_hid]
 
     def get_mem_cell(self, nth_para):
         return self.memory[nth_para]
 
     def save_memory(self):
-        torch.save({
-            'memory'        : self.memory,
-            'attn_memory'   : self.attn_mask
-        }, PATH['memory'])
+        torch.save(self.memory, PATH['memory'])
 
 
 class MemoryBasedReasoning(torch_nn.Module):
@@ -277,15 +280,13 @@ class MemoryBasedReasoning(torch_nn.Module):
         Y.append(final[:, :b, :].unsqueeze(0))
         Y   = torch.cat(Y, dim=0).to(args.device).permute(0, 2, 1, 3)
         # [n_paras + 1, b, seq_len_ques + seq_len_para, d_hid]
-        print(f"Y: {Y.shape}")
+
         Y   = Y.reshape(n_paras + 1, b, -1)
         # [n_paras+1, b, (seq_len_ques + seq_len_para)*d_hid]
 
         Y   = self.linearY(Y)
         # [n_paras + 1, b, d_hid]
 
-
-        print(f"Y = {Y.shape}")
 
         return Y
 